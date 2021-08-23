1277903db82cf0defaaf4ab82663cca8aaf70ada
zyrif <rubaiyat9@gmail.com>
2021-08-23 19:39:51 +0600

    Feat: Add login page

---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container fill-height>
+    <v-row align="center" justify="center">
+      <v-col cols="4">
+        <h1 class="text-center">BucketList Logo</h1>
+        <v-text-field placeholder="     Username">
+            <v-icon slot="prepend" class="mr-4">far fa-user</v-icon>
+        </v-text-field>
+        <v-text-field placeholder="     Password">
+            <v-icon slot="prepend" class="mr-4">fas fa-lock</v-icon>
+        </v-text-field>
+        <div class="text-center">
+          <v-btn outlined rounded>Sign in</v-btn>
+        </div>
+        <p class="text-center mt-4">or</p>
+        <div class="text-center">
+          <v-btn dark rounded>Sign up</v-btn>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass">
+
+</style>
