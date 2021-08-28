7815a41315f777cf19ddc5b2884866bcce919c4d
zyrif <rubaiyat9@gmail.com>
2021-08-29 01:06:39 +0600

    Add forget password dialog in signin page
    Co-authored-by: BeeZed <BeeZed@users.noreply.github.com>

--- src/components/SigninForm.vue
+++ src/components/SigninForm.vue
@@ -7,7 +7,13 @@
     <v-text-field placeholder="     Password">
       <v-icon slot="prepend" class="mr-4">fas fa-lock</v-icon>
     </v-text-field>
-    <div class="d-flex flex-row-reverse mb-4 text-caption">Forgot Password?</div>
+    <div
+      class="div-forgot-password d-flex flex-row-reverse mb-4 text-caption"
+      style="cursor: pointer"
+      @click="ForgetPasswordHandler"
+    >
+      Forgot Password?
+    </div>
     <div class="text-center">
       <v-btn outlined rounded>Sign in</v-btn>
     </div>
@@ -15,21 +21,42 @@
     <div class="text-center">
       <v-btn dark rounded @click="SignupHandler">Sign up</v-btn>
     </div>
+    <v-dialog v-model="isForgetPassOpen" max-width="450">
+      <v-card>
+        <v-card-title>Enter Your Registered Email Address: </v-card-title>
+        <v-card-text>
+          <v-text-field placeholder="Email" />
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn text> reset </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-col>
 </template>
 
 <script>
 export default {
-  name: 'SigninForm',
+  name: "SigninForm",
+  data: function () {
+    return {
+      isForgetPassOpen: false,
+    };
+  },
   methods: {
-    SignupHandler: function() {
-      this.$emit('signupClicked');
-    }
-  }
-
-}
+    SignupHandler: function () {
+      this.$emit("signupClicked");
+    },
+    ForgetPasswordHandler: function () {
+      this.isForgetPassOpen = !this.isForgetPassOpen;
+    },
+  },
+};
 </script>
 
 <style>
-
+.div-forgot-password:hover {
+  color: #42A5F5;
+}
 </style>
