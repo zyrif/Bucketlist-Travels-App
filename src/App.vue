a939d7072f26e4ea7e740a04602fc38d1ff90dee
zyrif <rubaiyat9@gmail.com>
2022-07-05 02:52:06 +0600

    Feat: Add user avatar

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,9 @@
       >
         Sign in
       </v-btn>
+      <v-avatar v-if="showAvatar" color="#264F49" class="my-auto">
+        <span class="white--text text-h6 font-weight-light">MR</span>
+      </v-avatar>
     </v-app-bar>
 
     <v-main v-bind="getVMainStyles">
@@ -35,6 +38,12 @@ export default {
     },
     showLogin: function () {
       return this.$route.name === "Login" ? false : true;
+    },
+    showAvatar: function () {
+      if (this.$route.name === "Home" || this.$route.name === "Login") {
+        return false;
+      }
+      return true;
     },
     getVAppStyles: function () {
       return {
