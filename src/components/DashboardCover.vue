891ac9811b77e87a47f7adb5826703e52c2bec46
zyrif <rubaiyat9@gmail.com>
2022-07-05 02:52:41 +0600

    Feat: Add cover image in dashboard

---
+++ src/components/DashboardCover.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-col style="padding: 0px">
+    <v-img v-bind="options"></v-img>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: "DashboardCover",
+  computed: {
+    options: function () {
+      return {
+        "aspect-ratio": 6.75/1,
+        src: require("../assets/Dashboard-BG.jpg")
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,15 @@
 <template>
-  <div></div>
+  <v-container fluid>
+    <v-row align="start">
+      <cover />
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import Cover from "../components/DashboardCover.vue"
 export default {
-
+  components: { Cover }
 }
 </script>
 
