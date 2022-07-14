1bb2a152c31595370dcb5d15dcebc1fc61ddcb4d
zyrif <rubaiyat9@gmail.com>
2022-07-14 19:28:28 +0600

    Fix: Enable dark mode for vuetify components...
    ...as the background is a dark image.

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,20 +1,20 @@
 <template>
   <v-col cols="4">
     <site-logo logotype="hero"/>
-    <v-text-field placeholder="     Username">
+    <v-text-field dark placeholder="     Username">
       <v-icon slot="prepend" class="mr-4">far fa-user</v-icon>
     </v-text-field>
-    <v-text-field placeholder="     Email">
+    <v-text-field dark placeholder="     Email">
       <v-icon slot="prepend" class="mr-4">far fa-envelope</v-icon>
     </v-text-field>
-    <v-text-field placeholder="     Password">
+    <v-text-field dark placeholder="     Password">
       <v-icon slot="prepend" class="mr-4">fas fa-lock</v-icon>
     </v-text-field>
-    <v-text-field placeholder="     Confirm Password">
+    <v-text-field dark placeholder="     Confirm Password">
       <v-icon slot="prepend" class="mr-4">fas fa-lock</v-icon>
     </v-text-field>
     <div class="text-center mt-8">
-      <v-btn outlined rounded>Sign Up</v-btn>
+      <v-btn dark outlined rounded>Sign Up</v-btn>
     </div>
   </v-col>
 </template>
