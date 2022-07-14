dd276d3790737287f972f638d9ff3fa0f477abc0
zyrif <rubaiyat9@gmail.com>
2022-07-14 19:26:01 +0600

    Feat: Add 'Confirm Password' field in Signup form

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -10,6 +10,9 @@
     <v-text-field placeholder="     Password">
       <v-icon slot="prepend" class="mr-4">fas fa-lock</v-icon>
     </v-text-field>
+    <v-text-field placeholder="     Confirm Password">
+      <v-icon slot="prepend" class="mr-4">fas fa-lock</v-icon>
+    </v-text-field>
     <div class="text-center mt-8">
       <v-btn outlined rounded>Sign Up</v-btn>
     </div>
