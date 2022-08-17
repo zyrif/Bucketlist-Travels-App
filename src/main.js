69f6ab3c46a3f6f1cdf98417165dc2b46da226c6
zyrif <rubaiyat9@gmail.com>
2022-08-18 03:50:07 +0600

    Feat: Create axios instance and make it available inside Vue

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$axios = axios.create({
+  baseURL: 'http://127.0.0.1:8000',
+  timeout: 1000
+})
 
 new Vue({
   router,
