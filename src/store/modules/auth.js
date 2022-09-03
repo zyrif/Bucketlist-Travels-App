d7a0bd6f7ab0f96a294a3845cf1462b9fe2ce485
zyrif <rubaiyat9@gmail.com>
2022-09-03 22:37:28 +0600

    Feat: Create 'auth' vuex module & implement functionality

---
+++ src/store/modules/auth.js
@@ -0,0 +1,68 @@
+const state = () => ({
+  user: {
+    id: '',
+    email: '',
+    auth_token: ''
+  }
+});
+
+const getters = {
+  getUserId: function() {
+    return this.user.id
+  },
+
+  getUserEmail: function() {
+    return this.user.email
+  }
+}
+
+const mutations = {
+  setUser: function(state, data) {
+    // if data is null, then set props to empty value
+    for (let key of Object.keys(state.user)) {
+      state.user[key] = ''
+    }
+
+    if (data) {
+      state.user.id = data.id
+      state.user.email = data.email
+    }
+  },
+  setUserAuthToken: function(state, auth_token) {
+    state.user.auth_token = auth_token
+  }
+}
+
+const actions = {
+  authenticate: function({ commit }, payload) {
+    return new Promise((resolve, reject) => {
+      const axios = this._vm.$axios
+      axios
+        .post('/auth/token/login/', {
+          email: payload.email,
+          password: payload.password
+        })
+        .then((resp) => {
+          if (resp.status === 200) {
+            commit('setUserAuthToken', resp.data.auth_token)
+            resolve(resp)
+          } else {
+            reject(resp)
+          }
+        }).catch((error) => {
+          reject(error)
+        }).finally(() => {
+          //
+        })
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
+}
+
