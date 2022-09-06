6ca383ef5bdd38762b9fbfc8dd047b73f392112b
zyrif <rubaiyat9@gmail.com>
2022-09-06 19:05:54 +0600

    Feat: Connect user registration logic with signup form

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,23 +1,23 @@
 <template>
   <v-col cols="4">
     <site-logo logotype="hero" />
-    <v-text-field dark placeholder="First Name">
+    <v-text-field v-model="form.firstName" dark placeholder="First Name">
       <v-icon slot="prepend" class="mr-4">far fa-user</v-icon>
     </v-text-field>
-    <v-text-field dark placeholder="Last Name">
+    <v-text-field v-model="form.lastName" dark placeholder="Last Name">
       <v-icon slot="prepend" class="mr-4">far fa-user</v-icon>
     </v-text-field>
-    <v-text-field dark placeholder="Email">
+    <v-text-field v-model="form.email" dark placeholder="Email" type="email">
       <v-icon slot="prepend" class="mr-4">far fa-envelope</v-icon>
     </v-text-field>
-    <v-text-field dark placeholder="Password">
+    <v-text-field v-model="form.password" dark placeholder="Password" type="password">
       <v-icon slot="prepend" class="mr-4">fas fa-lock</v-icon>
     </v-text-field>
-    <v-text-field dark placeholder="Confirm Password">
+    <v-text-field v-model="form.confirmPassword" dark placeholder="Confirm Password" type="password">
       <v-icon slot="prepend" class="mr-4">fas fa-lock</v-icon>
     </v-text-field>
     <div class="text-center mt-8">
-      <v-btn dark outlined rounded >Sign Up</v-btn>
+      <v-btn dark outlined rounded @click="signUpHandler">Sign Up</v-btn>
     </div>
     <p class="text-center white--text mt-4">or</p>
     <div class="text-center">
@@ -27,15 +27,50 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 import SiteLogo from '../components/SiteLogo.vue'
 
 export default {
   name: 'SignupForm',
+
   components: { SiteLogo },
+
+  data: function () {
+    return {
+      form: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      }
+    }
+  },
+
   methods: {
+    signUpHandler: function () {
+      if (!this.form.email || !this.form.password || !this.form.confirmPassword) {
+        return
+      }
+
+      this
+        .registerUser(this.form)
+        .then(() => {
+          this.signInHandler()
+        })
+        .catch(() => {
+          console.error("Sign Up Failed")
+        })
+        .finally(() => {
+          //
+        })
+    },
+
     signInHandler: function () {
       this.$emit('signinClicked')
-    }
+    },
+
+    ...mapActions('auth', ['registerUser'])
   }
 }
 </script>
