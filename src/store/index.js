523d4d8253e094189541b17a539e2086a59019fe
zyrif <rubaiyat9@gmail.com>
2022-09-14 09:22:04 +0600

    Feat(place): Init vuex module & implement fetch logic

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './modules/auth'
+import places from './modules/places'
 
 Vue.use(Vuex)
 
@@ -13,5 +14,6 @@ export default new Vuex.Store({
   },
   modules: {
     auth,
+    places
   }
 })
---
+++ src/store/modules/places.js
@@ -0,0 +1,62 @@
+import axios from '@/axiosConf'
+
+const state = () => ({
+  count: 0,
+  next: null,
+  previous: null,
+  results: [],
+})
+
+const getters = {
+  hasRemaining: function(state) {
+    if (state.count > 0 && state.next != null) {
+      return true
+    }
+    return false
+  },
+}
+
+const mutations = {
+  setData: function(state, payload = { append: false, data: {} }) {
+    state.count = payload.data.count || 0
+    state.next = payload.data.next || null
+    state.previous = payload.data.previous || null
+    if (payload.append && state.results.length > 0) {
+      state.results.concat(payload.data.results || [])
+    } else {
+      state.results = payload.data.results || []
+    }
+  },
+}
+
+const actions = {
+  fetchPlaces: function({ state, getters, commit }, payload = { next: true }) {
+    return new Promise((resolve, reject) => {
+      const fetchNext = payload.next && getters.hasRemaining
+      axios
+        .get(fetchNext ? state.next : "/place/")
+        .then((resp) => {
+          if (resp.status === 200) {
+            commit("setData", { append: fetchNext, data: resp.data })
+            resolve(resp.data)
+          } else {
+            reject(resp)
+          }
+        })
+        .catch((error) => {
+          reject(error)
+        })
+        .finally(() => {
+          //
+        })
+    })
+  },
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
+}
