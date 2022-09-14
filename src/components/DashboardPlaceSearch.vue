c62e2fcc2a812a4e3fc110f7a3db95291587cbd9
zyrif <rubaiyat9@gmail.com>
2022-09-14 10:56:45 +0600

    Style: Fix formatting in DashboardPlaceSearch.vue

--- src/components/DashboardPlaceSearch.vue
+++ src/components/DashboardPlaceSearch.vue
@@ -1,21 +1,8 @@
 <template>
-  <v-text-field
-    filled
-    outlined
-    rounded
-    background-color="white"
-    color="grey"
-    placeholder="Search a place you wanna go..."
-    class="ma-4"
-  >
+  <v-text-field filled outlined rounded background-color="white" color="grey"
+    placeholder="Search a place you wanna go..." class="ma-4">
     <template #append>
-      <v-btn
-        dark
-        rounded
-        color="white"
-        elevation="0"
-        style="top: -7px; left: 14px"
-      >
+      <v-btn dark rounded color="white" elevation="0" style="top: -7px; left: 14px">
         <v-icon dense color="grey">
           fas fa-search
         </v-icon>
@@ -31,5 +18,4 @@ export default {
 </script>
 
 <style>
-
 </style>
