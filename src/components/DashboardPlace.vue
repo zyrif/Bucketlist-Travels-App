7feb3f5f2e80e8d3d8b842748d08c0589e426590
zyrif <rubaiyat9@gmail.com>
2022-09-16 03:24:13 +0600

    Ref(place): Turn item card into it's own component

--- src/components/DashboardPlace.vue
+++ src/components/DashboardPlace.vue
@@ -1,42 +1,18 @@
 <template>
   <div style="display:contents">
-    <v-card v-for="place in places" v-bind:key="place.sortOrder" rounded="lg" color="grey lighten-4" elevation="0"
-      class="ma-4 pa-2">
-      <div class="d-flex">
-        <div>
-          <v-avatar tile color="grey" size="140" class="ma-4">
-          </v-avatar>
-        </div>
-        <div>
-          <v-card-title class="text-h5">
-            {{ place.name }}
-          </v-card-title>
-          <v-card-text class="black--text">
-            {{ place.description }}
-          </v-card-text>
-          <v-card-text>
-            <v-btn icon class="mx-2">
-              <v-icon dense color="black"> fas fa-map-marker-alt </v-icon>
-            </v-btn>
-            <v-btn icon class="mx-2">
-              <v-icon dense color="black"> fas fa-location-arrow </v-icon>
-            </v-btn>
-            <v-btn v-if="!myList && !place.visited" icon class="mx-2" @click="addToBucketListHandler(place)">
-              <v-icon dense color="black"> fas fa-plus-circle </v-icon>
-            </v-btn>
-            <v-btn v-if="!myList && place.visited" icon class="mx-2" @click="removeFromBucketListHandler(place)">
-              <v-icon dense color="black"> fas fa-check-circle </v-icon>
-            </v-btn>
-          </v-card-text>
-        </div>
-      </div>
-    </v-card>
+    <place-item v-for="place in places" v-bind:key="place.sortOrder" :place="place" :my-list="myList"
+      @add-visited="addToBucketListHandler" @remove-visited="removeFromBucketListHandler" />
   </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import PlaceItem from '@/components/DashboardPlaceItem.vue'
 export default {
+  components: {
+    PlaceItem
+  },
+
   props: {
     myList: Boolean,
   },
---
+++ src/components/DashboardPlaceItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-card rounded="lg" color="grey lighten-4" elevation="0"
+    class="ma-4 pa-2">
+    <div class="d-flex">
+      <div>
+        <v-avatar tile color="grey" size="140" class="ma-4">
+        </v-avatar>
+      </div>
+      <div>
+        <v-card-title class="text-h5">
+          {{ place.name }}
+        </v-card-title>
+        <v-card-text class="black--text">
+          {{ place.description }}
+        </v-card-text>
+        <v-card-text>
+          <v-btn icon class="mx-2">
+            <v-icon dense color="black"> fas fa-map-marker-alt </v-icon>
+          </v-btn>
+          <v-btn icon class="mx-2">
+            <v-icon dense color="black"> fas fa-location-arrow </v-icon>
+          </v-btn>
+          <v-btn v-if="!myList && !place.visited" icon class="mx-2" @click="$emit('add-visited', place)">
+            <v-icon dense color="black"> fas fa-plus-circle </v-icon>
+          </v-btn>
+          <v-btn v-if="!myList && place.visited" icon class="mx-2" @click="$emit('remove-visited', place)">
+            <v-icon dense color="black"> fas fa-check-circle </v-icon>
+          </v-btn>
+        </v-card-text>
+      </div>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    place: {
+      type: Object,
+      required: true,
+    },
+    "my-list": {
+      type: Boolean,
+      default: true,
+    }
+  }
+}
+</script>
+
+<style>
+</style>
