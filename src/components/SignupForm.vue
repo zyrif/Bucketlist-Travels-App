f78ed31adbfbcb03842bca26fbec1c3894c34c9d
zyrif <rubaiyat9@gmail.com>
2022-09-24 14:29:46 +0600

    Various (check description)
    - Feat: Show field related backend errors as validation errors to
      present the errors in a better way to the users.
    
    - Ref: Escapsulate the text fields in a form element to prevent browser
      warnings. This also allows to check if the overall form is in a valid
      state beofre submitting as opposed to checking each field directly,
      preventing duplicate checks.

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,21 +1,27 @@
 <template>
   <v-col cols="4">
     <site-logo logotype="hero" />
-    <v-text-field v-model="form.firstName" dark placeholder="First Name">
-      <v-icon slot="prepend" class="mr-4">far fa-user</v-icon>
-    </v-text-field>
-    <v-text-field v-model="form.lastName" dark placeholder="Last Name">
-      <v-icon slot="prepend" class="mr-4">far fa-user</v-icon>
-    </v-text-field>
-    <v-text-field v-model="form.email" dark placeholder="Email" :rules="[rules.required, rules.email]" type="email">
-      <v-icon slot="prepend" class="mr-4">far fa-envelope</v-icon>
-    </v-text-field>
-    <v-text-field v-model="form.password" dark placeholder="Password" :rules="[rules.required, rules.password]" type="password">
-      <v-icon slot="prepend" class="mr-4">fas fa-lock</v-icon>
-    </v-text-field>
-    <v-text-field v-model="form.confirmPassword" dark placeholder="Confirm Password" :rules="[rules.required]" type="password">
-      <v-icon slot="prepend" class="mr-4">fas fa-lock</v-icon>
-    </v-text-field>
+    <v-form v-model="form.isValid" ref="signup-form">
+      <v-text-field v-model="form.firstName" dark placeholder="First Name">
+        <v-icon slot="prepend" class="mr-4">far fa-user</v-icon>
+      </v-text-field>
+      <v-text-field v-model="form.lastName" dark placeholder="Last Name">
+        <v-icon slot="prepend" class="mr-4">far fa-user</v-icon>
+      </v-text-field>
+      <v-text-field v-model="form.email" dark :error="!!errorMessages.email" :error-messages="errorMessages.email"
+        placeholder="Email" :rules="[rules.required, rules.email]" type="email">
+        <v-icon slot="prepend" class="mr-4">far fa-envelope</v-icon>
+      </v-text-field>
+      <v-text-field v-model="form.password" dark :error="!!errorMessages.password"
+        :error-messages="errorMessages.password" placeholder="Password" :rules="[rules.required, rules.password]"
+        type="password">
+        <v-icon slot="prepend" class="mr-4">fas fa-lock</v-icon>
+      </v-text-field>
+      <v-text-field v-model="form.confirmPassword" dark :error="!!errorMessages.confirmPassword"
+        :error-messages="errorMessages.confirmPassword" placeholder="Confirm Password" type="password">
+        <v-icon slot="prepend" class="mr-4">fas fa-lock</v-icon>
+      </v-text-field>
+    </v-form>
     <div class="text-center mt-8">
       <v-btn dark outlined rounded :loading="this.states.isSignUpBtnLoading" @click="signUpHandler">Sign Up</v-btn>
     </div>
@@ -41,6 +47,15 @@ export default {
   data: function () {
     return {
       form: {
+        isValid: false,
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      },
+
+      errorMessages: {
         firstName: '',
         lastName: '',
         email: '',
@@ -54,9 +69,34 @@ export default {
     }
   },
 
+  watch: {
+    "form.email": function () {
+      if (this.errorMessages.email) {
+        this.errorMessages.email = ""
+      }
+    },
+    "form.password": function () {
+      if (this.errorMessages.password) {
+        this.errorMessages.password = ""
+      }
+      this.checkIfPasswordsMatch()
+    },
+    "form.confirmPassword": function () {
+      this.checkIfPasswordsMatch()
+    },
+  },
+
   methods: {
+    checkIfPasswordsMatch: function () {
+      if (this.errorMessages.confirmPassword && this.form.password === this.form.confirmPassword) {
+        this.errorMessages.confirmPassword = ""
+      } else {
+        this.errorMessages.confirmPassword = "Entered passwords do not match."
+      }
+    },
+
     signUpHandler: function () {
-      if (!this.form.email || !this.form.password || !this.form.confirmPassword) {
+      if (!this.form.isValid) {
         return
       }
 
@@ -68,21 +108,25 @@ export default {
         })
         .catch((error) => {
           if (error.response && (error.response.status >= 400 && error.response.status <= 499)) {
-            if (error.response.status === 400 && "non_field_errors" in error.response.data) {
+            let knownError = false
+            if ("email" in error.response.data) {
+              knownError = true
+              this.errorMessages.email = error.response.data.email.toString()
+            }
+            if ("password" in error.response.data) {
+              knownError = true
+              this.errorMessages.password = error.response.data.password.toString()
+            }
+            if ("non_field_errors" in error.response.data) {
+              knownError = true
               this.openMessageDialog({
                 title: "Signup Failed",
                 body: error.response.data.non_field_errors.toString(),
                 canChoose: false,
                 defaultBtnText: "Dismiss"
               })
-            } else if (error.response.status === 400 && "email" in error.response.data) {
-              this.openMessageDialog({
-                title: "Signup Failed",
-                body: error.response.data.email.toString(),
-                canChoose: false,
-                defaultBtnText: "Dismiss"
-              })
-            } else {
+            }
+            if (!knownError) {
               console.log(error.response.data)
               this.openMessageDialog({
                 title: "Unknown Error",
